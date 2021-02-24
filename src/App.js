dc59e2df1456d40d7a11063fb0013fe8b30cba3b
Atul2 <atulasrani12@gmail.com>
2021-02-24 10:32:40 +0530

    second comit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,40 @@
 import React, { Component } from 'react';
-import Button from '@material-ui/core/Button';
-
+import {TextField, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, AppBar,Button, Toolbar, IconButton,Typography, Drawer,ListItem, ListItemIcon, List, ListItemText } from "@material-ui/core";
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+import Menu from '@material-ui/icons/Menu';
 
 export default class App extends Component {
 
   constructor(props) {
     super(props);
-    console.log("App - Constructor called");
-    console.log(props);
     this.state = {
-      roll: "101"
+      show: false,
+      openModal: false
     };
+    this.close = this.close.bind(this);
+    this.toggleDrawer = this.toggleDrawer.bind(this);
+    this.showModal = this.showModal.bind(this);
+    this.hideModal = this.hideModal.bind(this);
+  
   }
+  close() {
+    this.setState({
+      show: false
+    });
+  }
+
+  toggleDrawer() {
+    this.setState({ show: true });
+  }
+
+  showModal() {
+    this.setState({ openModal:true });
+  };
+
+  hideModal(){
+    this.setState({ openModal:false });
+  };
 
   static getDerivedStateFromProps(props, state) {
     console.log("App-  Get Derived From PROPS methods");
@@ -27,7 +50,76 @@ export default class App extends Component {
   render() {
     return (
       <>
-        <Button variant="contained" color="primary">Material Click Me</Button>
+       <div style={{flexGrow: 1}}>
+      <AppBar position="sticky">
+            <Toolbar >
+              <IconButton edge="start" onClick={this.toggleDrawer} color="inherit" aria-label="menu">
+              <Menu />
+                </IconButton>
+                <Typography variant="h6" style={{flexGrow: 1}}>
+                News
+              </Typography>
+              <Button color="inherit" onClick={this.showModal} >Login</Button>
+            </Toolbar>
+          
+          </AppBar>
+          
+          <Drawer open={this.state.show}
+          onClose={this.close} >
+            <List style={{width:250}}>
+
+              <ListItem>
+              <ListItemIcon><InboxIcon/></ListItemIcon>
+                <ListItemText>Inbox    </ListItemText>
+              </ListItem>
+
+              <ListItem>
+              <ListItemIcon><MailIcon/></ListItemIcon>
+                <ListItemText>Send Email    </ListItemText>
+              </ListItem>
+
+
+              <ListItem>
+              <ListItemIcon><InboxIcon/></ListItemIcon>
+                <ListItemText>Inbox    </ListItemText>
+              </ListItem>
+
+              <ListItem>
+              <ListItemIcon><MailIcon/></ListItemIcon>
+                <ListItemText>Send Email    </ListItemText>
+              </ListItem>
+
+
+            </List>
+          </Drawer> 
+        </div>
+        
+
+        <Dialog open={this.state.openModal} onClose={this.hideModal} aria-labelledby="form-dialog-title">
+        <DialogTitle id="form-dialog-title">Subscribe</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            To subscribe to this website, please enter your email address here. We will send updates
+            occasionally.
+          </DialogContentText>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Email Address"
+            type="email"
+            fullWidth
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={this.hideModal} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={this.hideModal} color="primary">
+            Subscribe
+          </Button>
+        </DialogActions>
+      </Dialog>
       </>
     )
   }
